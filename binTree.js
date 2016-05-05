ae0da93c1b55cdd5e5ca5ecfba1e6b32fd879d07
alexeynekrasov <alexey.a.nekrasov@gmail.com>
2016-05-05 19:38:07 +0200

    Init application

---
+++ binTree.js
@@ -0,0 +1,163 @@
+'use strict';
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    /**
+     * Add a new element to the tree
+     * @param value
+     */
+    add(value) {
+        const node = {
+            value: value,
+            left: null,
+            right: null
+        };
+
+        if (!this.root) {
+            this.root = node;
+            return;
+        }
+
+        let current = this.root;
+
+        while (true) {
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = node;
+                    break;
+                } else {
+                    current = current.left;
+                }
+            } else if (value > current.value) {
+                if (current.right === null) {
+                    current.right = node;
+                    break;
+                } else {
+                    current = current.right;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Check if element exist in the tree
+     * @param value
+     * @returns {boolean}
+     */
+    contains(value) {
+        let current = this.root;
+
+        while (current) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Find tree node by value
+     * @param value
+     * @returns {Object} Tree node
+     */
+    find(value) {
+        let current = this.root;
+
+        while (current) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return current;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Remove tree node by value
+     * @param value
+     */
+    remove(value) {
+        const array = this.toArray();
+        const index = array.indexOf(value);
+
+        if (index > -1) {
+            array.splice(index, 1);
+
+            this.root = null;
+
+            array.forEach(item => this.add(item));
+        }
+    }
+
+    /**
+     * Run through binary tree and run callback per node
+     * @param {function} callback
+     */
+    traverse(callback) {
+        const inOrder = node => {
+            if (!node) {
+                return;
+            }
+
+            if (node.left !== null) {
+                inOrder(node.left);
+            }
+
+            if (node.right !== null) {
+                inOrder(node.right);
+            }
+
+            callback.call(this, node);
+        };
+
+        inOrder(this.root);
+    }
+
+    /**
+     * Get count of tree nodes
+     * @returns {number}
+     */
+    size() {
+        let length = 0;
+
+        this.traverse(node => length++);
+
+        return length;
+    }
+
+    /**
+     * Convert tree to array
+     * @returns {Array}
+     */
+    toArray() {
+        const result = [];
+
+        this.traverse(node => result.push(node.value));
+
+        return result;
+    }
+
+    /**
+     * Convert tree to string
+     * @returns {string}
+     */
+    toString() {
+        return this.toArray().toString();
+    }
+}
+
+module.exports = BinarySearchTree;
---
+++ tests/classTest.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const BinarySearchTree = require('../binTree');
+const expect = require('chai').expect;
+
+const tree = new BinarySearchTree();
+const arr = [3,4,51,2,52,5];
+
+const getRandomValue = () => arr[Math.floor(Math.random() * arr.length)];
+
+describe('Test binary tree', () => {
+    it('Adding new items', () => {
+        arr.forEach( item => tree.add(item));
+    });
+
+    it('Check size', () => {
+        expect(tree.size()).to.equal(arr.length);
+    });
+
+    it('Check root value correctness', () => {
+        expect(tree.root.value).to.equal(arr[0]);
+    });
+
+    it('Left values should be greater then parent', () => {
+        let current = tree.root;
+
+        while(current.left) {
+            expect(current.value).to.be.above(current.left.value);
+            current = current.left;
+        }
+    });
+
+    it('Right values should be smaller then parent', () => {
+        let current = tree.root;
+
+        while(current.right) {
+            expect(current.value).to.be.below(current.right.value);
+            current = current.right;
+        }
+    });
+
+    it('Check if tree contains', ()=>{
+        const value = getRandomValue();
+        expect(tree.contains(value)).to.be.true;
+    });
+
+    it('Searching node by value', () => {
+        const value = getRandomValue();
+        expect(tree.find(value).value).to.equal(value);
+    });
+
+    it('Remove random element', () => {
+        const value = getRandomValue();
+        expect(tree.size()).to.equal(arr.length);
+        tree.remove(value);
+        expect(tree.size()).to.equal(arr.length - 1);
+    });
+});
+
